e5dcfb2ad80780aac495801ea49228e9fe2e8860
CJ-Felicitas <cjbfelicitas@usep.edu.ph>
2024-04-12 02:55:25 +0800

    update

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+// Initialize Swiper
 var swiper = new Swiper('.blog-slider', {
-      spaceBetween: 30,
-      effect: 'fade',
-      loop: true,
-      mousewheel: {
-        invert: false,
-      },
-      // autoHeight: true,
-      pagination: {
-        el: '.blog-slider__pagination',
-        clickable: true,
-      }
-    });
+  spaceBetween: 30,
+  effect: 'fade',
+  loop: true,
+  mousewheel: {
+    invert: false,
+  },
+  pagination: {
+    el: '.blog-slider__pagination',
+    clickable: true,
+  }
+});
+
+// Handle click event for "READ MORE" buttons
+$('.blog-slider__button').on('click', function(e) {
+  e.preventDefault(); // Prevent default link behavior
+  
+  // Navigate to the next slide
+  swiper.slideNext();
+});
